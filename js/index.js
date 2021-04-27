0c59f7a2e071dca8e0ea9afeb59f6edc9b6d8633
bernardocsf <bernardocoelhoferreira@gmail.com>
2021-04-27 17:25:28 +0100

    ok

--- js/index.js
+++ js/index.js
@@ -1,18 +1,25 @@
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var btnclose = document.getElementById("close");
+// Get the modal
+var modal = document.getElementById('modal');
 
+// Get the button that opens the modal
+var btn = document.getElementById("btnNavRegistate");
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
 btn.onclick = function() {
-    modal.style.display = "block";
+    modal.classList.add('show');
 }
 
-btnclose.onclick = function() {
-    modal.style.display = "none";
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.classList.remove('show');
 }
 
+// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        modal.classList.remove('show');
     }
 }
