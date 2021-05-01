d98c43e97db35d4a5c5cc83892cfdf468647ec50
bernardocsf <bernardocoelhoferreira@gmail.com>
2021-05-01 19:14:31 +0100

    ok

--- js/index.js
+++ js/index.js
@@ -1,23 +1,12 @@
-// Get the modal
 var modal = document.getElementById('modal');
-
-// Get the button that opens the modal
 var btn = document.getElementById("btnNavRegistate");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
 btn.onclick = function() {
     modal.classList.add('show');
 }
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.classList.remove('show');
 }
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.classList.remove('show');
