beb243997a9a2ff30bc61085215c11cbe3671eb1
bernardocsf <bernardocoelhoferreira@gmail.com>
2021-05-05 12:27:35 +0100

    ok

--- js/utilizador.js
+++ js/utilizador.js
@@ -169,39 +169,19 @@ function perfilEditado() {
 
 
 
-// cards array holds all cards
+
 let card = document.getElementsByClassName("card");
 let cards = [...card];
-
-// deck of all cards in game
 const deck = document.getElementById("card-deck");
-
-// declaring move variable
 let moves = 0;
 let counter = document.querySelector(".moves");
-
-// declare variables for star icons
 const stars = document.querySelectorAll(".fa-star");
-
-// declaring variable of matchedCards
 let matchedCard = document.getElementsByClassName("match");
-
-// stars list
 let starsList = document.querySelectorAll(".stars li");
-
-// close icon in modal
 let closeicon = document.querySelector(".close");
-
-// declare modal
 let modal = document.getElementById("popup1")
-
-// array for opened cards
 var openedCards = [];
 
-
-// @description shuffles cards
-// @param {array}
-// @returns shuffledarray
 function shuffle(array) {
     var currentIndex = array.length,
         temporaryValue, randomIndex;
@@ -217,17 +197,11 @@ function shuffle(array) {
 };
 
 
-
-
-// @description function to start a new play 
 function startGame() {
 
-    // empty the openCards array
     openedCards = [];
-
-    // shuffle deck
     cards = shuffle(cards);
-    // remove all exisiting classes from each card
+
     for (var i = 0; i < cards.length; i++) {
         deck.innerHTML = "";
         [].forEach.call(cards, function(item) {
@@ -235,15 +209,15 @@ function startGame() {
         });
         cards[i].classList.remove("show", "open", "match", "disabled");
     }
-    // reset moves
+
     moves = 0;
     counter.innerHTML = moves;
-    // reset rating
+
     for (var i = 0; i < stars.length; i++) {
         stars[i].style.color = "#FFD700";
         stars[i].style.visibility = "visible";
     }
-    //reset timer
+
     second = 0;
     minute = 0;
     hour = 0;
@@ -252,16 +226,12 @@ function startGame() {
     clearInterval(interval);
 }
 
-
-// @description toggles open and show class to display cards
 var displayCard = function() {
     this.classList.toggle("open");
     this.classList.toggle("show");
     this.classList.toggle("disabled");
 };
 
-
-// @description add opened cards to OpenedCards list and check if cards are match or not
 function cardOpen() {
     openedCards.push(this);
     var len = openedCards.length;
@@ -275,8 +245,6 @@ function cardOpen() {
     }
 };
 
-
-// @description when cards match
 function matched() {
     openedCards[0].classList.add("match", "disabled");
     openedCards[1].classList.add("match", "disabled");
@@ -285,8 +253,6 @@ function matched() {
     openedCards = [];
 }
 
-
-// description when cards don't match
 function unmatched() {
     openedCards[0].classList.add("unmatched");
     openedCards[1].classList.add("unmatched");
@@ -299,16 +265,12 @@ function unmatched() {
     }, 1100);
 }
 
-
-// @description disable cards temporarily
 function disable() {
     Array.prototype.filter.call(cards, function(card) {
         card.classList.add('disabled');
     });
 }
 
-
-// @description enable cards and disable matched cards
 function enable() {
     Array.prototype.filter.call(cards, function(card) {
         card.classList.remove('disabled');
@@ -318,19 +280,17 @@ function enable() {
     });
 }
 
-
-// @description count player's moves
 function moveCounter() {
     moves++;
     counter.innerHTML = moves;
-    //start timer on first click
+
     if (moves == 1) {
         second = 0;
         minute = 0;
         hour = 0;
         startTimer();
     }
-    // setting rates based on moves
+
     if (moves > 8 && moves < 12) {
         for (i = 0; i < 3; i++) {
             if (i > 1) {
@@ -346,8 +306,6 @@ function moveCounter() {
     }
 }
 
-
-// @description game timer
 var second = 0;
 minute = 0;
 hour = 0;
@@ -369,31 +327,24 @@ function startTimer() {
     }, 1000);
 }
 
-
-// @description congratulations when all cards match, show modal and moves, time and rating
 function congratulations() {
     if (matchedCard.length == 16) {
         clearInterval(interval);
         finalTime = timer.innerHTML;
 
-        // show congratulations modal
         modal.classList.add("show");
 
-        // declare star rating variable
         var starRating = document.querySelector(".stars").innerHTML;
 
-        //showing move, rating, time on modal
         document.getElementById("finalMove").innerHTML = moves;
         document.getElementById("starRating").innerHTML = starRating;
         document.getElementById("totalTime").innerHTML = finalTime;
 
-        //closeicon on modal
         closeModal();
     };
 }
 
 
-// @description close icon on modal
 function closeModal() {
     closeicon.addEventListener("click", function(e) {
         modal.classList.remove("show");
@@ -401,15 +352,12 @@ function closeModal() {
     });
 }
 
-
-// @desciption for user to play Again 
 function playAgain() {
     modal.classList.remove("show");
     startGame();
 }
 
 
-// loop to add event listeners to each card
 for (var i = 0; i < cards.length; i++) {
     card = cards[i];
     card.addEventListener("click", displayCard);
